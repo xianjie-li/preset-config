e02457c0bbfb0260438ac67a3e30006a2dc70be2
lxj <1073830130@qq.com>
2020-03-21 19:27:30 +0800

    fix: 去掉部分规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,37 @@ module.exports = {
 
   rules: {
     /* js */
-    // 缩进为2
-    indent: ['error', 2],
-    // 只够使用单引号
-    quotes: ['error', 'single'],
-
-    'no-console': 'off',
-    // 使用分号
-    semi: ['error', 'always'],
-
+    // 代码格式用prettier
+    quotes: 'off',
+    indent: 'off',
+    // 会误伤@
     'import/no-unresolved': 'off',
+    
+    'no-unused-expressions': 'off',
+    // 必须要new
+    'no-new': 'off',
+    // 有时候需要声明局部变量
+    'no-underscore-dangle': 'off',
+    // 可以避免声明多余的变量
+    'no-param-reassign': 'off',
+    // 返回值有可能是联合类型
+    'consistent-return': 'off',
+    // 简化书写, 保证用 () 包裹即可
+    'no-return-assign': 'off',
+    // 这个要有
+    'no-plusplus': 'off',
+    // 很多时候会在文本中放emoji
+    'jsx-a11y/accessible-emoji': 'off',
     // ts
 
-    // react
+    /* react */
+    // 有时候需要使用dangerouslySetInnerHTML
+    'react/no-danger': 'off',
+    // 在没有id时只能使用index
+    'react/no-array-index-key': 'off',
+
+    // 其他
+
+    'eslint-comments/no-unlimited-disable': 'off',
   },
 };
